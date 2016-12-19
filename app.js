06ba75228699d0a32a7730f5edec7396ca6e6205
webdevthierry <thierry.hor.sd@gmail.com>
2016-12-19 07:37:05 +0100

    Basic files created

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser");
+
+
+
+app.listen(3000, function()
+{
+	console.log("Blog running!");
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Blog App
+
+This is a blog app using:
+* Html5
+* Css3
+* Semantic UI
+* NodeJS/ExpressJS
+* MongoDB
+
+* RESTful Architecture
