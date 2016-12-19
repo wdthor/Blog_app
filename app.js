e39f09afbdd893e3f7f8a20619ec921450a34d2b
webdevthierry <thierry.hor.sd@gmail.com>
2016-12-19 08:28:43 +0100

    Modules and basic setting added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var express = require("express"),
-	app = express(),
-	bodyParser = require("body-parser");
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser 	= require("body-parser"),
+	mongoose 	= require("mongoose");
 
+mongoose.connect("mongodb://localhost/blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.listen(3000, function()
