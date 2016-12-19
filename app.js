b6d2f65d741ffccc39e8f58cad5c0efab8cdadbd
webdevthierry <thierry.hor.sd@gmail.com>
2016-12-20 00:14:59 +0100

    Blog schema model added, cat images added, 'views' folder and index page created

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ var express 	= require("express"),
 	bodyParser 	= require("body-parser"),
 	mongoose 	= require("mongoose");
 
+// APP Config
 mongoose.connect("mongodb://localhost/blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
 
-app.listen(3000, function()
-{
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTFUL ROUTES
+
+app.get("/blogs", function(req, res){
+	res.render("index");
+});
+
+app.listen(3000, function(){
 	console.log("Blog running!");
 });
