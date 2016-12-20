6586d9d0035d244eaaf83501bf3865465d9432cd
webdevthierry <thierry.hor.sd@gmail.com>
2016-12-20 01:22:52 +0100

    partials added, 'index' and 'new' routes created

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express 	= require("express"),
 mongoose.connect("mongodb://localhost/blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.static("images"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Mongoose/Model Config
@@ -21,9 +22,27 @@ var Blog = mongoose.model("Blog", blogSchema);
 
 // RESTFUL ROUTES
 
-app.get("/blogs", function(req, res){
-	res.render("index");
+app.get("/", function(req, res){
+	res.redirect("/blogs");
 });
+
+// INDEX ROUTE
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log("ERROR!")
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	});
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+// CREATE ROUTE
 
 app.listen(3000, function(){
 	console.log("Blog running!");
